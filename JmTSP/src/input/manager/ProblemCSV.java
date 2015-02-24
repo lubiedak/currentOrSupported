711cdac0a1c66bc0ee0d5709bb9542efebe00c19
Łukasz Biedak <l.biedak@wp.pl>
2015-02-24 11:53:57 +0100

    Problem Creator Classes

--- JmTSP/src/input/manager/ProblemCSV.java
+++ JmTSP/src/input/manager/ProblemCSV.java
@@ -1,9 +1,32 @@
 package input.manager;
 
+import jmtsp.Problem;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  *
  * @author lbiedak
  */
-public class ProblemCSV {
+public class ProblemCSV 
+implements ProblemCreator {
+    String fileName;
+    
+    public ProblemCSV(String fileName)
+    {
+        this.fileName = fileName;
+    }
+    
+    public Problem CreateProblem()
+    {
+        
+        
+        return new Problem();
+    }
+    
+    public boolean CheckForDataCorectness()
+    {
+        return true;
+    }
     
 }
--- JmTSP/src/input/manager/ProblemCreator.java
+++ JmTSP/src/input/manager/ProblemCreator.java
@@ -1,9 +1,13 @@
 package input.manager;
-
+import jmtsp.ProblemRestrictions;
+import jmtsp.Problem;
 /**
  *
  * @author lbiedak
  */
 public interface ProblemCreator {
     
+    Problem CreateProblem();
+    
+    boolean CheckForDataCorectness();
 }
