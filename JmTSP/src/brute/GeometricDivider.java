ba27b4719eaf3b338f2c74fe364f2795ea3d1e64
Łukasz Biedak <l.biedak@wp.pl>
2015-04-16 07:42:31 +0100

    [JmTSP] Geometric Divider prototype

--- JmTSP/src/brute/GeometricDivider.java
+++ JmTSP/src/brute/GeometricDivider.java
@@ -1,9 +1,38 @@
 package brute;
 
+import BasicTypes.Point;
+import java.util.ArrayList;
+
 /**
  *
  * @author lbiedak
  */
 public class GeometricDivider {
     
+    Point[] points;
+    int n;
+    Point middle;
+    
+    public GeometricDivider(Point[] points, int n){
+        middle = points[0];
+        this.n = n;
+        this.points = new Point[points.length - 1];
+        for(int i = 1; i < points.length; ++i){
+            this.points[i-1] = points[i];
+        }
+    }
+    
+    public GeometricDivider(Point[] points, int n, Point middle){
+        this.points = points;
+        this.n = n;
+        this.middle = middle;
+    }
+    
+    public ArrayList<Point[]> Divide(int sensitivity){
+        
+        return new ArrayList<Point[]>();
+    }
+    
+    
 }
+
